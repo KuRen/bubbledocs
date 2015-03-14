9026086ba9f1fc613f0e7217a5f1e827ea9175ce
bukjir <carlosalvc@gmail.com>
2015-03-14 18:43:49 +0000

    Added getOwnSpreadsheetsByName() to User

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
@@ -1,11 +1,27 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.ArrayList;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.Atomic.TxMode;
+
 public class User extends User_Base {
-    
+
     public User(String username, String password, String name) {
         super();
         setUsername(username);
         setPassword(password);
         setName(name);
+    }
+
+    @Atomic(mode = TxMode.READ)
+    public ArrayList<Spreadsheet> getOwnSpreadsheetsByName(String name) {
+        ArrayList<Spreadsheet> list = new ArrayList<Spreadsheet>();
+
+        for (Spreadsheet s : getSpreadsheetsSet())
+            if (s.getName().equals(name))
+                list.add(s);
+
+        return list;
     }
 }
