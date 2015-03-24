dacdc82ffb0ef01fc9c2181571272adbf446d620
Joao Francisco Vieira Goncalves <ist176547@linbase.rnl.tecnico.ulisboa.pt>
2015-03-24 13:04:53 +0000

    Added test files and new exceptions

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/DuplicateUsernameException.java
@@ -0,0 +1,10 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class DuplicateUsernameException extends BubbleDocsException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/UnknownBubbleDocsUserException.java
@@ -0,0 +1,10 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class UnknownBubbleDocsUserException extends BubbleDocsException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
