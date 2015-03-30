6829fa078f9a6a344e3b255829aeab167d4d1608
bukjir <carlosalvc@gmail.com>
2015-03-30 03:11:24 +0100

    First rework of ExportSpreadsheetService. (unfinished)

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportSpreadsheetService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportSpreadsheetService.java
@@ -5,17 +5,30 @@ import java.io.UnsupportedEncodingException;
 import org.jdom2.Document;
 import org.jdom2.output.XMLOutputter;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.Permission;
 import pt.tecnico.bubbledocs.domain.Spreadsheet;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 import pt.tecnico.bubbledocs.exception.ExportDocumentException;
+import pt.tecnico.bubbledocs.exception.InvalidArgumentException;
+import pt.tecnico.bubbledocs.exception.InvalidSpreadSheetIdException;
+import pt.tecnico.bubbledocs.exception.NonExistingSpreadsheetException;
+import pt.tecnico.bubbledocs.exception.UnauthorizedUserException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
 import pt.tecnico.bubbledocs.xml.BaseXMLWriter;
 import pt.tecnico.bubbledocs.xml.XMLWriter;
 
 public class ExportSpreadsheetService extends BubbleDocsService {
 
     private Spreadsheet spreadsheet;
+    private int spreadsheetId;
     private XMLWriter writer;
     private byte[] document;
+    private String userToken;
+
+    public ExportSpreadsheetService(int Id, String token) {
+        this(Id, new BaseXMLWriter(), token);
+    }
 
     public ExportSpreadsheetService(Spreadsheet ss) {
         this(ss, new BaseXMLWriter());
@@ -26,11 +39,43 @@ public class ExportSpreadsheetService extends BubbleDocsService {
         writer = xmlWriter;
     }
 
+    public ExportSpreadsheetService(int Id, XMLWriter xmlWriter, String token) {
+        spreadsheetId = Id;
+        writer = xmlWriter;
+        userToken = token;
+    }
+
     @Override
     protected void dispatch() throws BubbleDocsException {
+
+        if (spreadsheetId <= 0) {
+            throw new InvalidSpreadSheetIdException();
+        }
+
+        Spreadsheet ss = BubbleDocs.getInstance().getSpreadsheetById(spreadsheetId);
+
+        if (ss == null) {
+            throw new NonExistingSpreadsheetException();
+        }
+
+        if (userToken == null || userToken.isEmpty()) {
+            throw new InvalidArgumentException("The auth token can't be empty");
+        }
+
+        String username = getBubbleDocs().getManager().findUserByToken(userToken);
+
+        if (username == null) {
+            throw new UserNotInSessionException();
+        }
+
+        Permission permission = ss.findPermissionsForUser(username);
+
+        if (permission == null && !(ss.getOwner().getUsername().equals(username)))
+            throw new UnauthorizedUserException();
+
         Document jdomDoc = new Document();
 
-        jdomDoc.setRootElement(spreadsheet.accept(writer));
+        jdomDoc.setRootElement(ss.accept(writer));
 
         XMLOutputter xml = new XMLOutputter();
         try {
---
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/ExportSpreadsheetServiceTest.java
@@ -0,0 +1,173 @@
+package pt.tecnico.bubbledocs.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import pt.tecnico.bubbledocs.domain.Addition;
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.Cell;
+import pt.tecnico.bubbledocs.domain.Literal;
+import pt.tecnico.bubbledocs.domain.Reference;
+import pt.tecnico.bubbledocs.domain.Spreadsheet;
+import pt.tecnico.bubbledocs.exception.ImportDocumentException;
+import pt.tecnico.bubbledocs.exception.InvalidArgumentException;
+import pt.tecnico.bubbledocs.exception.InvalidSpreadSheetIdException;
+import pt.tecnico.bubbledocs.exception.NonExistingSpreadsheetException;
+import pt.tecnico.bubbledocs.exception.TokenExpiredException;
+import pt.tecnico.bubbledocs.exception.UnauthorizedUserException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+
+public class ExportSpreadsheetServiceTest extends BubbleDocsServiceTest {
+
+    private String authorizedToken;
+    private String unauthorizedToken;
+    private int validId;
+
+    private static final int VALUE = 2;
+    private static final int ROWS = 30;
+    private static final int COLS = 40;
+    private static final int CELL_1_COL = 1;
+    private static final int CELL_1_ROW = 1;
+    private static final int CELL_2_COL = 2;
+    private static final int CELL_2_ROW = 2;
+
+    private static final int INVALID_ID = -42;
+    private static final int NOT_EXISTENT_ID = 42000;
+    private static final String EMPTY_TOKEN = "";
+    private static final String NOT_IN_SESSION_TOKEN = "not in session!!!";
+    private static final String USER = "userName";
+    private static final String SS_NAME = "A SS Name";
+
+    @Test(expected = InvalidSpreadSheetIdException.class)
+    public void invalidSpreadsheet() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(INVALID_ID, authorizedToken);
+        service.execute();
+    }
+
+    @Test(expected = NonExistingSpreadsheetException.class)
+    public void nonExistingSpreadsheet() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(NOT_EXISTENT_ID, authorizedToken);
+        service.execute();
+    }
+
+    @Test(expected = InvalidArgumentException.class)
+    public void emptyToken() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(validId, EMPTY_TOKEN);
+        service.execute();
+    }
+
+    @Test
+    @Ignore("The token format isn't known yet")
+    public void invalidToken() {
+
+    }
+
+    @Test(expected = UserNotInSessionException.class)
+    public void UserNotInSession() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(validId, NOT_IN_SESSION_TOKEN);
+        service.execute();
+    }
+
+    @Test(expected = TokenExpiredException.class)
+    public void expiredToken() {
+        expireToken(authorizedToken);
+        ExportSpreadsheetService service = new ExportSpreadsheetService(validId, authorizedToken);
+        service.execute();
+    }
+
+    @Test(expected = UnauthorizedUserException.class)
+    public void unauthUser() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(validId, unauthorizedToken);
+        service.execute();
+    }
+
+    @Test
+    public void success() {
+        ExportSpreadsheetService service = new ExportSpreadsheetService(validId, authorizedToken);
+        service.execute();
+
+        byte[] result = service.getResult();
+
+        Document doc;
+
+        SAXBuilder builder = new SAXBuilder();
+        builder.setIgnoringElementContentWhitespace(true);
+
+        try {
+            doc = builder.build(new ByteArrayInputStream(result));
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+            throw new ImportDocumentException();
+        }
+
+        Element rootElement = doc.getRootElement();
+
+        assertEquals(USER, rootElement.getAttribute("owner").getValue());
+        assertEquals(ROWS, Integer.parseInt(rootElement.getAttribute("rows").getValue()));
+        assertEquals(COLS, Integer.parseInt(rootElement.getAttribute("columns").getValue()));
+        assertEquals(SS_NAME, rootElement.getAttribute("name").getValue());
+
+        Element cells = rootElement.getChild("Cells");
+
+        assertEquals(cells.getChildren("Cell").size(), 2);
+
+        for (Element cellElement : cells.getChildren("Cell")) {
+            Integer row = Integer.parseInt(cellElement.getAttribute("row").getValue());
+            Integer column = Integer.parseInt(cellElement.getAttribute("column").getValue());
+
+            if (row == CELL_1_ROW && column == CELL_1_COL) {
+                assertEquals("Literal", cellElement.getChildren().get(0).getName());
+                assertEquals(VALUE, Integer.parseInt(cellElement.getChildren().get(0).getAttribute("literal").getValue()));
+            }
+
+            else if (row == CELL_2_ROW && column == CELL_2_COL) {
+                assertEquals("Addition", cellElement.getChildren().get(0).getName());
+
+                Element arg1Element = cellElement.getChildren().get(0).getChildren().get(0);
+                Element arg2Element = cellElement.getChildren().get(0).getChildren().get(1);
+
+                assertEquals("Reference", arg1Element.getName());
+                assertEquals("Literal", arg2Element.getName());
+
+                Element arg1CellElement = arg1Element.getChildren().get(0);
+
+                assertEquals(CELL_1_ROW, Integer.parseInt(arg1CellElement.getAttribute("row").getValue()));
+                assertEquals(CELL_1_COL, Integer.parseInt(arg1CellElement.getAttribute("column").getValue()));
+
+                assertEquals("Literal", arg2Element.getName());
+                assertEquals(VALUE, Integer.parseInt(arg2Element.getAttribute("literal").getValue()));
+            }
+            //TODO: ASSERT FAIL IN OTHER CASES
+        }
+    }
+
+    @Override
+    public void populate4Test() {
+        createUser(USER, "password", "big complete name");
+        authorizedToken = addUserToSession(USER);
+
+        createUser("otherUser", "password", "yet a bigger name");
+        unauthorizedToken = addUserToSession("otherUser");
+
+        BubbleDocs bd = BubbleDocs.getInstance();
+
+        Spreadsheet ss = createSpreadSheet(bd.getUserByUsername(USER), SS_NAME, ROWS, COLS);
+        validId = ss.getId();
+        Cell c1 = new Cell(ss, CELL_1_ROW, CELL_1_COL);
+        Cell c2 = new Cell(ss, CELL_2_ROW, CELL_2_COL);
+        c1.setContent(new Literal(VALUE));
+        c2.setContent(new Addition(new Reference(c1), new Literal(VALUE)));
+
+        ss.addCells(c1);
+        ss.addCells(c2);
+    }
+}
