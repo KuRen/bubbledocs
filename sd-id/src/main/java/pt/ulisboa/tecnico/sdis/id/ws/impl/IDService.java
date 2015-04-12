25bd094c77ad3e12ca30af70d4585d5eef9fe53f
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2015-04-12 18:32:20 +0100

    Backend implementation

---
+++ sd-id/src/main/java/pt/ulisboa/tecnico/sdis/id/ws/impl/IDService.java
@@ -0,0 +1,46 @@
+package pt.ulisboa.tecnico.sdis.id.ws.impl;
+
+import javax.jws.WebService;
+
+import pt.ulisboa.tecnico.sdis.id.ws.AuthReqFailed_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.SDId;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
+
+@SuppressWarnings("restriction")
+@WebService(endpointInterface = "pt.ulisboa.tecnico.sdis.id.ws.SDId", wsdlLocation = "SD-ID.1_1.wsdl", name = "SdId",
+        portName = "SDIDImplPort", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:id:ws", serviceName = "SDID")
+public class IDService implements SDId {
+
+    public void createUser(String userId, String emailAddress) throws EmailAlreadyExists_Exception, InvalidEmail_Exception,
+            InvalidUser_Exception, UserAlreadyExists_Exception {
+        UserManager userManager = UserManager.getInstance();
+        User user = userManager.create(userId, emailAddress);
+        System.out.printf("User %s created with password: %s%n", user.getUsername(), user.getPassword());
+    }
+
+    public void renewPassword(String userId) throws UserDoesNotExist_Exception {
+        UserManager userManager = UserManager.getInstance();
+        User user = userManager.renewPassword(userId);
+        System.out.printf("User %s password renewed to: %s%n", user.getUsername(), user.getPassword());
+    }
+
+    public void removeUser(String userId) throws UserDoesNotExist_Exception {
+        UserManager userManager = UserManager.getInstance();
+        userManager.remove(userId);
+    }
+
+    public byte[] requestAuthentication(String userId, byte[] reserved) throws AuthReqFailed_Exception {
+        UserManager userManager = UserManager.getInstance();
+        boolean success = userManager.authenticate(userId, reserved);
+        if (success == true)
+            return new byte[] { 1 };
+        else {
+            return new byte[] { 0 };
+        }
+    }
+
+}
---
+++ sd-id/src/main/java/pt/ulisboa/tecnico/sdis/id/ws/impl/UserManager.java
@@ -0,0 +1,138 @@
+package pt.ulisboa.tecnico.sdis.id.ws.impl;
+
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import pt.ulisboa.tecnico.sdis.id.ws.AuthReqFailed;
+import pt.ulisboa.tecnico.sdis.id.ws.AuthReqFailed_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists;
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
+
+public class UserManager {
+
+    private static UserManager instance = null;
+    private Map<String, User> users = new HashMap<String, User>();
+
+    protected UserManager() {
+
+    }
+
+    public static UserManager getInstance() {
+        if (instance == null)
+            instance = new UserManager();
+        return instance;
+    }
+
+    public User create(String username, String email, String password) throws EmailAlreadyExists_Exception,
+            InvalidEmail_Exception, InvalidUser_Exception, UserAlreadyExists_Exception {
+        validateUsername(username);
+        validateEmail(email);
+        if (password == null) {
+            password = generatePassword();
+        }
+        User user = new User(username, password, email);
+        users.put(username, user);
+        return user;
+    }
+
+    private String generatePassword() {
+        char[] characters =
+                new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                        'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+                        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        return randomString(characters, 8);
+    }
+
+    public String randomString(char[] characterSet, int length) {
+        Random random = new SecureRandom();
+        char[] result = new char[length];
+        for (int i = 0; i < result.length; i++) {
+            // picks a random index out of character set > random character
+            int randomCharIndex = random.nextInt(characterSet.length);
+            result[i] = characterSet[randomCharIndex];
+        }
+        return new String(result);
+    }
+
+    public User create(String username, String email) throws EmailAlreadyExists_Exception, InvalidEmail_Exception,
+            InvalidUser_Exception, UserAlreadyExists_Exception {
+        return create(username, email, null);
+    }
+
+    private void validateEmail(String email) throws InvalidEmail_Exception, EmailAlreadyExists_Exception {
+        if (!email.matches("[\\w\\._]+@(\\w+\\.)*\\w+\\.\\w+")) {
+            InvalidEmail faultInfo = new InvalidEmail();
+            faultInfo.setEmailAddress(email);
+            throw new InvalidEmail_Exception("Invalid email: " + email, faultInfo);
+        }
+        for (User user : users.values()) {
+            if (user.getEmail().equals(email)) {
+                EmailAlreadyExists faultInfo = new EmailAlreadyExists();
+                faultInfo.setEmailAddress(email);
+                throw new EmailAlreadyExists_Exception("Email " + email + " already exists", faultInfo);
+            }
+        }
+    }
+
+    private void validateUsername(String username) throws InvalidUser_Exception, UserAlreadyExists_Exception {
+        if (!username.matches(".+")) {
+            InvalidUser faultInfo = new InvalidUser();
+            faultInfo.setUserId(username);
+            throw new InvalidUser_Exception("Invalid username: " + username, faultInfo);
+        }
+        if (users.get(username) != null) {
+            UserAlreadyExists faultInfo = new UserAlreadyExists();
+            faultInfo.setUserId(username);
+            throw new UserAlreadyExists_Exception("User " + username + " already exists", faultInfo);
+        }
+    }
+
+    public User renewPassword(String username, String password) throws UserDoesNotExist_Exception {
+        User user = users.get(username);
+        if (user == null) {
+            UserDoesNotExist faultInfo = new UserDoesNotExist();
+            faultInfo.setUserId(username);
+            throw new UserDoesNotExist_Exception("User does not exist: " + username, faultInfo);
+        }
+        if (password == null) {
+            password = generatePassword();
+        }
+        user.setPassword(password);
+        return user;
+    }
+
+    public User renewPassword(String username) throws UserDoesNotExist_Exception {
+        return renewPassword(username, null);
+    }
+
+    public void remove(String username) throws UserDoesNotExist_Exception {
+        User user = users.get(username);
+        if (user == null) {
+            UserDoesNotExist faultInfo = new UserDoesNotExist();
+            faultInfo.setUserId(username);
+            throw new UserDoesNotExist_Exception("User does not exist: " + username, faultInfo);
+        }
+        users.remove(username);
+    }
+
+    public boolean authenticate(String username, byte[] reserved) throws AuthReqFailed_Exception {
+        User user = users.get(username);
+        if (user == null) {
+            AuthReqFailed faultInfo = new AuthReqFailed();
+            faultInfo.setReserved(reserved);
+            throw new AuthReqFailed_Exception("Authentication failed", faultInfo);
+        }
+        String password = new String(reserved);
+        return user.getPassword().equals(password);
+    }
+}
