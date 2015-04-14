a9f594089783a754d12ca853c8db2c865a2c43e4
Rodrig0v <RVRodrigo1@hotmail.com>
2015-04-14 23:22:30 +0100

    closes #29

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
@@ -1,20 +1,46 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.Spreadsheet;
+import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
+import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
+import pt.tecnico.bubbledocs.service.remote.StoreRemoteServices;
 
 public class ExportDocument extends BubbleDocsService {
     private byte[] docXML;
+    private String userToken;
+    private int docId;
 
     public byte[] getDocXML() {
         return docXML;
     }
 
     public ExportDocument(String userToken, int docId) {
-        // add code here
+        this.userToken = userToken;
+        this.docId = docId;
     }
 
     @Override
     protected void dispatch() throws BubbleDocsException {
-        // add code here
+    	
+    	ExportSpreadsheetService service = new ExportSpreadsheetService(docId,userToken);
+        service.dispatch();
+        docXML = service.getResult();
+        
+        Spreadsheet ss = BubbleDocs.getInstance().getSpreadsheetById(docId);
+        
+        User user = getBubbleDocs().getSessionManager().findUserByToken(userToken);
+        
+        StoreRemoteServices remote = new StoreRemoteServices();
+        
+        try {
+        	remote.storeDocument(user.getName(), ss.getName(), docXML);
+        } catch(RemoteInvocationException rie) {
+        	throw new UnavailableServiceException();
+        }
+        
+        refreshToken(userToken);
     }
 }
