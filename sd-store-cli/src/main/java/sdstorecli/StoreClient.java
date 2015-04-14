a21fcda7dc1080b061c0d9dff60a9e0602e496b7
KuRen <kurenjr@kuren.org>
2015-04-14 23:38:26 +0100

    Added StoreClient

--- sd-store-cli/src/main/java/sdstorecli/StoreClient.java
+++ sd-store-cli/src/main/java/sdstorecli/StoreClient.java
@@ -1,59 +1,83 @@
 package sdstorecli;
 
+import java.util.List;
 import java.util.Map;
 
-import javax.xml.ws.*;
+import javax.xml.ws.BindingProvider;
 
-import sdstorecli.uddi.UDDINaming;
-import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
-import pt.ulisboa.tecnico.sdis.store.ws.*; // classes generated from WSDL
+import pt.ulisboa.tecnico.sdis.store.ws.CapacityExceeded_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocDoesNotExist_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocUserPair;
+import pt.ulisboa.tecnico.sdis.store.ws.SDStore;
+import pt.ulisboa.tecnico.sdis.store.ws.SDStore_Service;
+import pt.ulisboa.tecnico.sdis.store.ws.UserDoesNotExist_Exception;
 
+public class StoreClient implements SDStore {
+    /** WS service */
+    SDStore_Service service = null;
 
-public class StoreClient {
-	
-    public static void main(String[] args) throws Exception {
-        // Check arguments
-        if (args.length < 2) {
-            System.err.println("Argument(s) missing!");
-            System.err.printf("Usage: java %s uddiURL name%n", StoreClient.class.getName());
-            return;
-        }
+    /** WS port (interface) */
+    SDStore port = null;
 
-        String uddiURL = args[0];
-        String name = args[1];
+    /** WS endpoint address */
+    // default value is defined by WSDL
+    private String wsURL = null;
 
-        System.out.printf("Contacting UDDI at %s%n", uddiURL);
-        UDDINaming uddiNaming = new UDDINaming(uddiURL);
+    /** output option **/
+    private boolean verbose = false;
 
-        System.out.printf("Looking for '%s'%n", name);
-        String endpointAddress = uddiNaming.lookup(name);
-
-        if (endpointAddress == null) {
-            System.out.println("Not found!");
-            return;
-        } else {
-            System.out.printf("Found %s%n", endpointAddress);
-        }
-
-        System.out.println("Creating stub ...");
-        SDStore_Service service = new SDStore_Service();
-        SDStore port = service.getSDStoreImplPort();
-
-        System.out.println("Setting endpoint address ...");
-        BindingProvider bindingProvider = (BindingProvider) port;
-        Map<String, Object> requestContext = bindingProvider.getRequestContext();
-        requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
-
-        DocUserPair pair = new DocUserPair();
-        pair.setDocumentId("HelloDoc");
-        pair.setUserId("MrHello");
-        System.out.println("Creating file HelloDoc by MrHello ...");
-        port.createDoc(pair);
-        System.out.println("Storing string Hello in file HelloDoc by MrHello ...");
-        port.store(pair, new String("Hello").getBytes());
-        System.out.println("Loading file HelloDoc by MrHello ...");
-        byte[] result = port.load(pair);
-        System.out.println("File Content: " + new String(result));
+    public boolean isVerbose() {
+        return verbose;
     }
-    
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    /** constructor with provided web service URL */
+    public StoreClient(String wsURL) throws StoreClientException {
+        this.wsURL = wsURL;
+        createStub();
+    }
+
+    /** default constructor uses default endpoint address */
+    public StoreClient() throws StoreClientException {
+        createStub();
+    }
+
+    /** Stub creation and configuration */
+    protected void createStub() {
+        if (verbose)
+            System.out.println("Creating stub ...");
+        service = new SDStore_Service();
+        port = service.getSDStoreImplPort();
+
+        if (wsURL != null) {
+            if (verbose)
+                System.out.println("Setting endpoint address ...");
+            BindingProvider bindingProvider = (BindingProvider) port;
+            Map<String, Object> requestContext = bindingProvider.getRequestContext();
+            requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsURL);
+        }
+    }
+
+    // SDStore
+
+    public void createDoc(DocUserPair docUserPair) throws DocAlreadyExists_Exception {
+        port.createDoc(docUserPair);
+    }
+
+    public List<String> listDocs(String userId) throws UserDoesNotExist_Exception {
+        return port.listDocs(userId);
+    }
+
+    public void store(DocUserPair docUserPair, byte[] contents) throws CapacityExceeded_Exception, DocDoesNotExist_Exception,
+            UserDoesNotExist_Exception {
+        port.store(docUserPair, contents);
+    }
+
+    public byte[] load(DocUserPair docUserPair) throws DocDoesNotExist_Exception, UserDoesNotExist_Exception {
+        return port.load(docUserPair);
+    }
 }
---
+++ sd-store-cli/src/main/java/sdstorecli/StoreClientException.java
@@ -0,0 +1,24 @@
+package sdstorecli;
+
+public class StoreClientException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public StoreClientException() {
+    }
+
+    public StoreClientException(String message) {
+        super(message);
+    }
+
+    public StoreClientException(Throwable cause) {
+        super(cause);
+    }
+
+    public StoreClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
