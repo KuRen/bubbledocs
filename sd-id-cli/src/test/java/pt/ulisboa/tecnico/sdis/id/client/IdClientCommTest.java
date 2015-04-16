5b8821b03fd12eb2aaff206fcf3c8e8033f1d23b
bukjir <carlosalvc@gmail.com>
2015-04-16 21:21:01 +0100

    Added some more comm tests

--- sd-id-cli/src/test/java/pt/ulisboa/tecnico/sdis/id/client/IdClientCommTest.java
+++ sd-id-cli/src/test/java/pt/ulisboa/tecnico/sdis/id/client/IdClientCommTest.java
@@ -1,9 +1,16 @@
 package pt.ulisboa.tecnico.sdis.id.client;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import javax.xml.registry.JAXRException;
+import javax.xml.ws.WebServiceException;
 
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 import mockit.Mocked;
+import mockit.NonStrictExpectations;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,10 +19,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import pt.ulisboa.tecnico.sdis.id.uddi.UDDINaming;
+import pt.ulisboa.tecnico.sdis.id.ws.SDId;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
 
 public class IdClientCommTest {
 
     IdClient client = null;
+    private static int x = 1;
 
     @BeforeClass
     public static void oneTimeSetUp() {
@@ -43,7 +53,7 @@ public class IdClientCommTest {
      * @throws serviceFindException
      */
     @Test(expected = serviceFindException.class)
-    public void testMockServerException(@Mocked final UDDINaming uddi) throws JAXRException, serviceFindException {
+    public void testMockServerCommunicationException(@Mocked final UDDINaming uddi) throws JAXRException, serviceFindException {
         new Expectations() {
             {
                 new UDDINaming(anyString);
@@ -55,6 +65,73 @@ public class IdClientCommTest {
         client = new IdClient("some.url", "Some-service-name");
     }
 
+    @Test(expected = UserAlreadyExists_Exception.class)
+    public <T extends SDId> void testMockServerException(@Mocked final UDDINaming uddi) throws Exception {
+
+        new MockUp<T>() {
+            @Mock
+            void createUser(String name, String email) throws UserAlreadyExists_Exception {
+                throw new UserAlreadyExists_Exception("fabricated", null);
+            }
+        };
+
+        new NonStrictExpectations() {
+            {
+                new UDDINaming(anyString);
+                uddi.lookup(anyString);
+                result = "http://localhost:8081";
+            }
+        };
+
+        // Unit under test is exercised.
+        IdClient client = new IdClient("http://localhost:8081", "sd-id");
+        client.createUser("user", "usermail@mail.com");
+    }
+
+    /**
+     * In this test the server is mocked to
+     * simulate a communication exception on a second call.
+     */
+    @Test
+    public <T extends SDId> void testMockServerExceptionOnSecondCall(@Mocked final UDDINaming uddi) throws Exception {
+
+        new MockUp<T>() {
+            @Mock
+            void createUser(String name, String email) {
+                if (x == 2)
+                    throw new WebServiceException("fabricated");
+                x++;
+            }
+        };
+
+        new NonStrictExpectations() {
+            {
+                new UDDINaming(anyString);
+                uddi.lookup(anyString);
+                result = "http://localhost:8081";
+            }
+        };
+
+        // Unit under test is exercised.
+        IdClient client = new IdClient("http://localhost:8081", "sd-id");
+
+        // first call to mocked server
+        try {
+            client.createUser("user", "usermail@mail.com");
+        } catch (WebServiceException e) {
+            // exception is not expected
+            fail();
+        }
+
+        // second call to mocked server
+        try {
+            client.createUser("user2", "usermail2@mail.com");
+            fail();
+        } catch (WebServiceException e) {
+            // exception is expected
+            assertEquals("fabricated", e.getMessage());
+        }
+    }
     /*
     @Test
     public <M extends SDId & BindingProvider & Closeable> void success(@Mocked final UDDINaming uddi,
