54d56c542b668d3a54ce68552e318ea188e56a07
KuRen <kurenjr@kuren.org>
2015-04-30 16:26:18 +0100

    closes #60 closes #61

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/GetUserInfoService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/GetUserInfoService.java
@@ -1,13 +1,26 @@
 package pt.tecnico.bubbledocs.service.local;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 public class GetUserInfoService extends BubbleDocsService {
 
-    @Override
-    protected void dispatch() throws BubbleDocsException {
-        // TODO Auto-generated method stub
+    private String username;
+    private User user;
 
+    public GetUserInfoService(String username) {
+        this.username = username;
     }
 
+    @Override
+    protected void dispatch() throws BubbleDocsException {
+        BubbleDocs bubbleDocs = getBubbleDocs();
+
+        this.user = bubbleDocs.getUserByUsername(username);
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/GetUsernameForTokenService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/GetUsernameForTokenService.java
@@ -1,13 +1,33 @@
 package pt.tecnico.bubbledocs.service.local;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.SessionManager;
+import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 public class GetUsernameForTokenService extends BubbleDocsService {
 
+    private String token;
+    private String username;
+
+    public GetUsernameForTokenService(String token) {
+        this.token = token;
+    }
+
     @Override
     protected void dispatch() throws BubbleDocsException {
-        // TODO Auto-generated method stub
+        BubbleDocs bubbleDocs = getBubbleDocs();
+        SessionManager sessionManager = bubbleDocs.getSessionManager();
 
+        sessionManager.cleanOldSessions();
+
+        User user = sessionManager.findUserByToken(token);
+
+        this.username = user.getUsername();
+    }
+
+    public String getUsername() {
+        return username;
     }
 
 }
