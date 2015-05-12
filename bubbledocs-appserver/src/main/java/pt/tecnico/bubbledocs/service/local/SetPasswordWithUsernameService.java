63d437b9d02474fffcc173e7f5242dc38930f64d
DSil <daniel-sil@hotmail.com>
2015-05-12 18:47:23 +0100

    Fixed bug in SetPasswordWithUsernameService

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/SetPasswordWithUsernameService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/local/SetPasswordWithUsernameService.java
@@ -1,7 +1,9 @@
 package pt.tecnico.bubbledocs.service.local;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
 import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.exception.InvalidUsernameException;
 
 public class SetPasswordWithUsernameService extends BubbleDocsService {
 
@@ -15,7 +17,11 @@ public class SetPasswordWithUsernameService extends BubbleDocsService {
 
     @Override
     protected void dispatch() throws BubbleDocsException {
-        User user = getBubbleDocs().getUserByUsername(username);
+        BubbleDocs bubbleDocs = BubbleDocs.getInstance();
+        User user = bubbleDocs.getUserByUsername(username);
+
+        if (user == null)
+            throw new InvalidUsernameException();
 
         user.setPassword(password);
 
