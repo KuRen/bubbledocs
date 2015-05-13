5b7bedad8fe61f55e42820e849c27596a9ce767c
Rodrig0v <RVRodrigo1@hotmail.com>
2015-05-13 15:32:50 +0100

    fixed exception

--- sd-store-cli/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/client/StoreClient.java
+++ sd-store-cli/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/client/StoreClient.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.sdis.store.ws.client;
 
 import java.util.List;
+import pt.ulisboa.tecnico.sdis.store.ws.CapacityExceeded_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.DocAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocDoesNotExist_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.DocUserPair;
 import pt.ulisboa.tecnico.sdis.store.ws.UserDoesNotExist_Exception;
 
@@ -23,11 +25,30 @@ public class StoreClient {
         return frontend.listDocs(userId);
     }
 
-    public void store(DocUserPair docUserPair, byte[] contents) throws Throwable {
-        frontend.store(docUserPair, contents);
+    public void store(DocUserPair docUserPair, byte[] contents) throws UserDoesNotExist_Exception, DocDoesNotExist_Exception {
+        try {
+            frontend.store(docUserPair, contents);
+        } catch(UserDoesNotExist_Exception u) {
+            throw u;
+        } catch(DocDoesNotExist_Exception d) {
+            throw d;
+        } catch(Throwable t) {
+            System.out.println("Unknown Error");
+        }
     }
 
-    public byte[] load(DocUserPair docUserPair) throws Throwable {
-        return frontend.load(docUserPair);
+    public byte[] load(DocUserPair docUserPair) throws UserDoesNotExist_Exception, DocDoesNotExist_Exception, CapacityExceeded_Exception {
+        try {
+            return frontend.load(docUserPair);
+        } catch(UserDoesNotExist_Exception u) {
+            throw u;
+        } catch(DocDoesNotExist_Exception d) {
+            throw d;
+        } catch(CapacityExceeded_Exception c) {
+            throw d;
+        } catch(Throwable t) {
+            System.out.println("Unknown Error");
+            return null;
+        }
     }
 }
